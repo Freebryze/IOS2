7ec117de578fca2aeb9377549a362381c6dd9093
Freebryze <mathieu.combaz@gmail.com>
2021-02-24 17:33:44 +0100

    maj ViewController

--- RickAndMortyProject/CollectionViewController.swift
+++ RickAndMortyProject/CollectionViewController.swift
@@ -7,82 +7,78 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class CollectionViewController: UICollectionViewController {
 
+    private enum Section {
+        case main
+    }
+    
+    private enum Item: Hashable {
+        case character(SerieCharacter)
+    }
+    
+    private var diffableDataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.collectionViewLayout = createLayout()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        diffableDataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell? in
+            switch item {
+            case .character(let serieCharacter):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+                cell.textLabel?.text = serieCharacter.name
+                cell.imageView?.loadImage(from: serieCharacter.imageURL) {
+                    cell.setNeedsLayout()
+                }
+                cell.detailTextLabel?.text = DateFormatter.localizedString(from: serieCharacter.createdDate,
+                                                                           dateStyle: .medium,
+                                                                           timeStyle: .short)
+            }
+        })
 
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
+        let snapshot = createSnapshot()
+        diffableDataSource.apply(snapshot)
+        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
+
+private func createLayout() -> UICollectionViewLayout {
+    let layout = UICollectionViewCompositionalLayout(sectionProvider: { (section, environment) -> NSCollectionLayoutSection? in
+        let snapshot = self.diffableDataSource.snapshot()
+        let currentSection = snapshot.sectionIdentifiers[section]
+        
+        switch currentSection {
+        case .main:
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .absolute(50))
+
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                         subitem: item,
+                                                         count: 2)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 10
+
+            return section
+        }
+    })
+
+    return layout
+}
+
+    private func createSnapshot() -> NSDiffableDataSourceSnapshot<Section, Item> {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(mainColorItems, toSection: .main)
+
+        return snapshot
+    }
